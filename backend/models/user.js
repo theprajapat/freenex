4dd006f5a554565f8eeb1f3b6798909c97910c74
Himanshu Kumar <142046523+theprajapat@users.noreply.github.com>
2024-11-18 18:54:51 +0530

    Delete backend directory

--- backend/models/user.js
+++
@@ -1,8 +0,0 @@
-const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    skills: { type: [String] },  // This should be an array of strings
-    location: { type: String }
-  });
-  
